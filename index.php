952ea8c4a6b22814ba1ef81d7ce8a47ab990f3b7
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-01-13 16:50:59 +0700

    init project

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include "templates/head.php"; ?>
+
+<body>
+  <?php include "templates/header.php"; ?>
+     <h1>BODY</h1>
+  <?php include "templates/footer.php"; ?>
+  <script>
+   
+  </script>
+</body>
+
+</html>
---
+++ templates/footer.php
@@ -0,0 +1,12 @@
+<footer class="main-footer">
+	<h1>FOOOTER</h1>
+</footer>
+<!-- /.main-footer -->
+
+<!-- JAVASCRIPT-->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/main.js"></script>
