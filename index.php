9531542915db07053f37deb04dbcf59d11d7c599
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-01-14 19:12:29 +0700

    create-banner

--- index.php
+++ index.php
@@ -2,6 +2,39 @@
 
 <body>
     <?php include "templates/header.php"; ?>
+    <div class="home-page__wrapper">
+        <div class="banner-wrapper">
+            <div class="container">
+                <div class="top-banner">
+                    <div class="content-banner">
+                        <h1 class="title-banner">Data-driven infection control</h1>
+                        <p class="description-banner">
+                            At NEX.Q, develop and deliver AI-based solutions to control outbreaks of hospital infections
+                            and antimicrobial resistance across healthcare facilities.
+                        </p>
+                    </div>
+                    <span class="working-with-label-pc">Working with</span>
+                </div>
+                <div class="list-connect-branch__wrapper">
+                    <span class="working-with-label-mb">Working with</span>
+                    <div class="list-connect-branch">
+                        <div class="branch-item">
+                            <img src="./images/branch-1.png" alt="">
+                        </div>
+                        <div class="branch-item">
+                            <img src="./images/branch-2.png" alt="">
+                        </div>
+                        <div class="branch-item">
+                            <img src="./images/branch-3.png" alt="">
+                        </div>
+                        <div class="branch-item">
+                            <img src="./images/branch-4.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php include "templates/footer.php"; ?>
 </body>
 
